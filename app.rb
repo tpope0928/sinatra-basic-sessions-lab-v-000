fd740bc7ca2a454f2966f9412f18988ec342eb30
Thomas Pope <tpope0928@gmail.com>
2019-02-25 17:38:19 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,4 +1,23 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  configure do 
+    enable :sessions
+    set :session_secret, "phish"
+  end
+
+  get '/' do
+    erb :index
+  end
+
+  post '/checkout' do
+    session[:item] = params[:item]
+
+    @sessions = session
+    @params = params
+
+    erb :item
+  end
+  
 end
